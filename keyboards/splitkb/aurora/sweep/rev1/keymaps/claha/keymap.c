acec795ccbaa9c3a39b100112281a3672a6dc022
Claes Hallström <hallstrom.claes@gmail.com>
2025-09-01 15:48:45 +0200

    aurora/sweep: turn off power led

--- keyboards/splitkb/aurora/sweep/rev1/keymaps/claha/keymap.c
+++ keyboards/splitkb/aurora/sweep/rev1/keymaps/claha/keymap.c
@@ -15,6 +15,7 @@
  */
 
 #include QMK_KEYBOARD_H
+#include "gpio.h"
 
 #define HOME_A SFT_T(KC_A)
 #define HOME_S CTL_T(KC_S)
@@ -70,3 +71,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 )
 
 };
+
+void keyboard_pre_init_user(void) {
+    gpio_set_pin_output(24);
+    gpio_write_pin_high(24);
+}
