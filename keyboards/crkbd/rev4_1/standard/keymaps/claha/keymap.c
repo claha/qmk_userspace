b195754e73fb122fe7ac526474c81022c17fb4c5
Claes Hallström <hallstrom.claes@gmail.com>
2025-10-23 21:54:18 +0200

    crkbd: align with aurora/sweep

--- keyboards/crkbd/rev4_1/standard/keymaps/claha/keymap.c
+++ keyboards/crkbd/rev4_1/standard/keymaps/claha/keymap.c
@@ -17,34 +17,58 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include QMK_KEYBOARD_H
+#include "keymap_swedish.h"
+
+#define HOME_A SFT_T(KC_A)
+#define HOME_S CTL_T(KC_S)
+#define HOME_D GUI_T(KC_D)
+#define HOME_F ALT_T(KC_F)
+#define HOME_J ALT_T(KC_J)
+#define HOME_K GUI_T(KC_K)
+#define HOME_L CTL_T(KC_L)
+#define HOME_SCLN SFT_T(KC_NO)
+
+#define THUMB_L2 KC_NO
+#define THUMB_L1 LT(3, KC_TAB)
+#define THUMB_L0 LT(1, KC_SPC)
+#define THUMB_R0 LT(4, KC_ENT)
+#define THUMB_R1 LT(2, KC_BSPC)
+#define THUMB_R2 KC_NO
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_split_3x6_3(
-      KC_ESC,    KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,             KC_Y,           KC_U,            KC_I,           KC_O,          KC_P,          KC_BSPC,
-      KC_TAB,    SFT_T(KC_A),    CTL_T(KC_S),    GUI_T(KC_D),    ALT_T(KC_F),    KC_G,             KC_H,           ALT_T(KC_J),     GUI_T(KC_K),    CTL_T(KC_L),   SFT_T(KC_NO),  KC_ENT,
-      KC_NO,     KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,             KC_N,           KC_M,            KC_COMM,        KC_DOT,        KC_SLSH,       KC_NO,
-                                                 KC_NO,          LT(3, KC_TAB),  LT(1, KC_SPC),    KC_ENT,         LT(2, KC_BSPC),  KC_NO
+  [0] = LAYOUT_split_3x6_3(
+    KC_NO,    KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_NO,
+    KC_NO,    HOME_A,   HOME_S,   HOME_D,   HOME_F,   KC_G,     KC_H,     HOME_J,   HOME_K,   HOME_L,   HOME_SCLN,KC_NO,
+    KC_NO,    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_NO,
+                                  THUMB_L2, THUMB_L1, THUMB_L0, THUMB_R0, THUMB_R1, THUMB_R2
   ),
 
-    [1] = LAYOUT_split_3x6_3(
-      KC_TRNS,   KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,             KC_NO,         KC_NO,           KC_NO,    	    KC_NO,         KC_NO,         KC_TRNS,
-      KC_TRNS,   KC_LSFT,        KC_LCTL,        KC_LGUI,        KC_LALT,        KC_NO,             KC_LEFT,       KC_DOWN,         KC_UP,    	    KC_RIGHT,      KC_NO,         KC_TRNS,
-      KC_TRNS,   KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,             KC_HOME,       KC_PGDN,         KC_PGUP,  	    KC_END,        KC_NO,         KC_TRNS,
-                                                 KC_TRNS,        KC_TRNS,        KC_TRNS,           KC_TRNS,       KC_TRNS,         KC_TRNS
+  [1] = LAYOUT_split_3x6_3(
+    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+    KC_NO,    KC_LSFT,  KC_LCTL,  KC_LGUI,  KC_LALT,  KC_NO,    KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, KC_NO,    KC_NO,
+    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   KC_NO,    KC_NO,
+                                  KC_TRNS,  KC_ESC,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
   ),
 
-    [2] = LAYOUT_split_3x6_3(
-      KC_TRNS,   KC_1,           KC_2,           KC_3,           KC_4,           KC_5,              KC_6,         KC_7,             KC_8,           KC_9,          KC_0,          KC_TRNS,
-      KC_TRNS,   KC_LSFT,        KC_NUHS,        KC_NUBS,        KC_MINUS,       KC_NO,             KC_NO,        KC_LALT,          KC_LGUI,        KC_LCTL,       KC_LSFT,       KC_TRNS,
-      KC_TRNS,   KC_RALT,        KC_DOT,         KC_RBRC,        KC_PLUS,        KC_NO,             KC_NO,        KC_LBRC,          KC_QUOT,        KC_SCLN,       KC_RALT,       KC_TRNS,
-                                                 KC_TRNS,        KC_ESC,         KC_TRNS,           KC_TRNS,      KC_TRNS,          KC_TRNS
+  [2] = LAYOUT_split_3x6_3(
+    KC_NO,    KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_NO,
+    KC_NO,    KC_LSFT,  KC_NUHS,  KC_NUBS,  KC_MINUS, KC_NO,    KC_NO,    KC_LALT,  KC_LGUI,  KC_LCTL,  KC_LSFT,  KC_NO,
+    KC_NO,    KC_RALT,  KC_DOT,   KC_RBRC,  KC_PLUS,  KC_NO,    KC_NO,    KC_LBRC,  KC_QUOT,  KC_SCLN,  KC_RALT,  KC_NO,
+                                  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
   ),
 
-    [3] = LAYOUT_split_3x6_3(
-      KC_NO,    QK_BOOT,         KC_NO,          KC_NO,          KC_NO,          KC_NO,             KC_NO,        KC_NO,            KC_NO,         KC_NO,         KC_NO,          KC_NO,
-      KC_NO,    RM_TOGG,         RM_HUEU,        RM_SATU,        RM_VALU,        KC_NO,             KC_NO,        KC_NO,            KC_NO,         KC_NO,         KC_NO,          KC_NO,
-      KC_NO,    RM_NEXT,         RM_HUED,        RM_SATD,        RM_VALD,        KC_NO,             KC_NO,        KC_NO,            KC_NO,         KC_NO,         KC_NO,          KC_NO,
-                                                 KC_NO,          KC_TRNS,        KC_NO,             KC_NO,        KC_NO,            KC_NO
+  [3] = LAYOUT_split_3x6_3(
+    KC_NO,    KC_NO,    KC_NO,    SE_LABK,  SE_RABK,  SE_PIPE,  KC_NO,    SE_LCBR,  SE_RCBR,  KC_NO,    KC_NO,    KC_NO,
+    KC_NO,    SE_TILD,  SE_ASTR,  SE_LBRC,  SE_RBRC,  KC_NO,    SE_QUOT,  SE_LPRN,  SE_RPRN,  SE_SCLN,  SE_DQUO,  KC_NO,
+    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    SE_COMM,  SE_DOT,   SE_MINS,  KC_NO,
+                                  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
+  ),
+
+  [4] = LAYOUT_split_3x6_3(
+    KC_NO,    QK_BOOT,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+    KC_NO,    RM_TOGG,  RM_HUEU,  RM_SATU,  RM_VALU,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+    KC_NO,    RM_NEXT,  RM_HUED,  RM_SATD,  RM_VALD,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+                                  KC_NO,    KC_TRNS,  KC_NO,    KC_NO,    KC_NO,    KC_NO
   )
 };
 
@@ -52,3 +76,17 @@ const uint16_t PROGMEM combo_capsword[] = {KC_B, KC_N, COMBO_END};
 combo_t key_combos[] = {
   COMBO(combo_capsword, CW_TOGG),
 };
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+  case SE_TILD:
+    if (record->event.pressed) {
+      register_code(KC_RALT);
+      tap_code(KC_RBRC);
+      unregister_code(KC_RALT);
+      tap_code(KC_SPC);
+    }
+            return false;
+  }
+  return true;
+}
