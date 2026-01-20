0b251f963aac203bff0b6494054f098a8bfd61d1
Claes Hallström <hallstrom.claes@gmail.com>
2026-01-20 20:39:37 +0100

    aurora/sweep: use enum for layers

--- keyboards/splitkb/aurora/sweep/rev1/keymaps/claha/keymap.c
+++ keyboards/splitkb/aurora/sweep/rev1/keymaps/claha/keymap.c
@@ -17,6 +17,7 @@
 #include QMK_KEYBOARD_H
 #include "keymap_swedish.h"
 #include "gpio.h"
+#include "claha.h"
 
 #define HOME_A SFT_T(KC_A)
 #define HOME_S CTL_T(KC_S)
@@ -27,49 +28,49 @@
 #define HOME_L CTL_T(KC_L)
 #define HOME_SCLN SFT_T(KC_NO)
 
-#define THUMB_L1 LT(3, KC_TAB)
-#define THUMB_L0 LT(1, KC_SPC)
-#define THUMB_R0 LT(4, KC_ENT)
-#define THUMB_R1 LT(2, KC_BSPC)
+#define THUMB_L1 LT(SYM, KC_TAB)
+#define THUMB_L0 LT(NAV, KC_SPC)
+#define THUMB_R0 LT(FUN, KC_ENT)
+#define THUMB_R1 LT(NUM, KC_BSPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-[0] = LAYOUT(
+[BASE] = LAYOUT(
     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,
     HOME_A,   HOME_S,   HOME_D,   HOME_F,   KC_G,     KC_H,     HOME_J,   HOME_K,   HOME_L,   HOME_SCLN,
     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,
                                   THUMB_L1, THUMB_L0, THUMB_R0, THUMB_R1
 ),
 
-[1] = LAYOUT(
+[NAV] = LAYOUT(
     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
     KC_LSFT,  KC_LCTL,  KC_LGUI,  KC_LALT,  KC_NO,    KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, KC_NO,
     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   KC_DEL,
                                   KC_ESC,   KC_TRNS,  KC_TRNS,  KC_TRNS
 ),
 
-[2] = LAYOUT(
+[NUM] = LAYOUT(
     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,
     KC_LSFT,  KC_NUHS,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_LALT,  KC_LGUI,  KC_LCTL,  KC_LSFT,
     KC_RALT,  KC_DOT,   KC_RBRC,  KC_PLUS,  KC_NO,    KC_NO,    KC_LBRC,  KC_QUOT,  KC_SCLN,  KC_RALT,
                                   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
 ),
 
-[3] = LAYOUT(
+[SYM] = LAYOUT(
     SE_BSLS,  SE_LABK,  SE_RABK,  SE_MINS,  SE_PIPE,  SE_CIRC,  SE_LCBR,  SE_RCBR,  SE_DLR,   SE_QUES,
     SE_EXLM,  SE_ASTR,  SE_SLSH,  SE_EQL,   SE_AMPR,  SE_HASH,  SE_LPRN,  SE_RPRN,  SE_SCLN,  SE_DQUO,
     SE_TILD,  SE_PLUS,  SE_LBRC,  SE_RBRC,  SE_PERC,  SE_AT,    SE_COLN,  SE_COMM,  SE_DOT,   SE_QUOT,
                                   KC_NO,    KC_NO,    KC_TRNS,  KC_TRNS
 ),
 
-[4] = LAYOUT(
+[FUN] = LAYOUT(
     KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,
     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_F11,   KC_F12,   KC_NO,    KC_NO,    KC_NO,    KC_NO,
     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
                                   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
 ),
 
-[5] = LAYOUT(
+[SYS] = LAYOUT(
     QK_BOOT,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
     RM_TOGG,  RM_HUEU,  RM_SATU,  RM_VALU,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
     RM_NEXT,  RM_HUED,  RM_SATD,  RM_VALD,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
@@ -86,7 +87,7 @@ const uint16_t PROGMEM combo_capsword[] = {KC_B, KC_N, COMBO_END};
 const uint16_t PROGMEM combo_syslayer[] = {THUMB_R0, THUMB_R1, COMBO_END};
 combo_t key_combos[] = {
   COMBO(combo_capsword, CW_TOGG),
-  COMBO(combo_syslayer, MO(5)),
+  COMBO(combo_syslayer, MO(SYS)),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
