db07687a737f9f37dbb2323b9bef07a406abc708
Claes Hallström <hallstrom.claes@gmail.com>
2026-02-20 20:24:49 +0100

    kprepublic/cstc40: sync with cheapino

--- keyboards/kprepublic/cstc40/rev3/keymaps/claha/keymap.c
+++ keyboards/kprepublic/cstc40/rev3/keymaps/claha/keymap.c
@@ -1,79 +1,95 @@
-/* Copyright 2023
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
+#include "keymap_swedish.h"
+#include "claha.h"
 
-#define QHME_L3 SFT_T(KC_A)
-#define QHME_L2 CTL_T(KC_S)
-#define QHME_L1 GUI_T(KC_D)
-#define QHME_L0 ALT_T(KC_F)
-#define QHME_R0 ALT_T(KC_J)
-#define QHME_R1 GUI_T(KC_K)
-#define QHME_R2 CTL_T(KC_L)
-#define QHME_R3 SFT_T(KC_NO)
+#define HOME_A SFT_T(KC_A)
+#define HOME_S CTL_T(KC_S)
+#define HOME_D GUI_T(KC_D)
+#define HOME_F ALT_T(KC_F)
+#define HOME_J ALT_T(KC_J)
+#define HOME_K GUI_T(KC_K)
+#define HOME_L CTL_T(KC_L)
+#define HOME_SCLN SFT_T(KC_NO)
 
-#define CHME_L3 SFT_T(KC_A)
-#define CHME_L2 CTL_T(KC_R)
-#define CHME_L1 GUI_T(KC_S)
-#define CHME_L0 ALT_T(KC_T)
-#define CHME_R0 ALT_T(KC_N)
-#define CHME_R1 GUI_T(KC_E)
-#define CHME_R2 CTL_T(KC_I)
-#define CHME_R3 SFT_T(KC_O)
-
-#define THMB_L1 LT(4, KC_TAB)
-#define THMB_L0 LT(2, KC_SPC)
-#define THMB_R0 KC_ENT
-#define THMB_R1 LT(3, KC_BSPC)
+#define THUMB_L1 LT(SYM, KC_TAB)
+#define THUMB_L0 LT(NAV, KC_SPC)
+#define THUMB_R0 LT(FUN, KC_ENT)
+#define THUMB_R1 LT(NUM, KC_BSPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-[0] = LAYOUT_planck_mit(
+  [BASE] = LAYOUT_planck_mit(
     KC_ESC,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
-    KC_TAB,   QHME_L3,  QHME_L2,  QHME_L1,  QHME_L0,  KC_G,     KC_H,     QHME_R0,  QHME_R1,  QHME_R2,  QHME_R3,  KC_ENT,
+    KC_TAB,   HOME_A,   HOME_S,   HOME_D,   HOME_F,   KC_G,     KC_H,     HOME_J,   HOME_K,   HOME_L,   HOME_SCLN,KC_ENT,
     XXXXXXX,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  THMB_L1,  THMB_L0,            THMB_R0,  THMB_R1,  XXXXXXX,  XXXXXXX,  XXXXXXX
-),
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  THUMB_L1, THUMB_L0,           THUMB_R0, THUMB_R1, XXXXXXX,  XXXXXXX,  XXXXXXX
+  ),
 
-[1] = LAYOUT_planck_mit(
-    KC_ESC,   KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,     KC_J,     KC_L,     KC_U,     KC_Y,     XXXXXXX,  KC_BSPC,
-    KC_TAB,   CHME_L3,  CHME_L2,  CHME_L1,  CHME_L0,  KC_G,     KC_M,     CHME_R0,  CHME_R1,  CHME_R2,  CHME_R3,  KC_ENT,
-    XXXXXXX,  KC_Z,     KC_X,     KC_C,     KC_D,     KC_V,     KC_K,     KC_H,     KC_COMM,  KC_DOT,   KC_SLSH,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  THMB_L1,  THMB_L0,            THMB_R0,  THMB_R1,  XXXXXXX,  XXXXXXX,  XXXXXXX
-),
+  [NAV] = LAYOUT_planck_mit(
+    KC_TRNS,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_TRNS,
+    KC_TRNS,  KC_LSFT,  KC_LCTL,  KC_LGUI,  KC_LALT,  KC_NO,    KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, KC_NO,    KC_TRNS,
+    KC_TRNS,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   KC_DEL,   KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
+  ),
 
-[2] = LAYOUT_planck_mit(
-    _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,
-    _______,  KC_LSFT,  KC_LCTL,  KC_LGUI,  KC_LALT,  XXXXXXX,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  XXXXXXX,  _______,
-    _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   XXXXXXX,  _______,
-    _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,  _______,  _______
-),
+  [NUM] = LAYOUT_planck_mit(
+    KC_TRNS,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_TRNS,
+    KC_TRNS,  KC_LSFT,  KC_NUHS,  KC_NUBS,  KC_MINS,  KC_NO,    KC_NO,    KC_LALT,  KC_LGUI,  KC_LCTL,  KC_LSFT,  KC_TRNS,
+    KC_TRNS,  KC_RALT,  KC_DOT,   KC_RBRC,  KC_PLUS,  KC_NO,    KC_NO,    KC_LBRC,  KC_QUOT,  KC_SCLN,  KC_RALT,  KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
+  ),
 
-[3] = LAYOUT_planck_mit(
-    _______,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     _______,
-    _______,  KC_LSFT,  KC_NUHS,  KC_NUBS,  KC_MINS,  XXXXXXX,  XXXXXXX,  KC_LALT,  KC_LGUI,  KC_LCTL,  KC_LSFT,  _______,
-    _______,  KC_RALT,  KC_DOT,   KC_RBRC,  KC_PLUS,  XXXXXXX,  XXXXXXX,  KC_LBRC,  KC_QUOT,  KC_SCLN,  KC_RALT,  _______,
-    _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,  _______,  _______   
-),
+  [SYM] = LAYOUT_planck_mit(
+    KC_TRNS,  SE_BSLS,  SE_LABK,  SE_RABK,  SE_MINS,  SE_PIPE,  SE_CIRC,  SE_LCBR,  SE_RCBR,  SE_DLR,   SE_QUES,  KC_TRNS,
+    KC_TRNS,  SE_EXLM,  SE_ASTR,  SE_SLSH,  SE_EQL,   SE_AMPR,  SE_HASH,  SE_LPRN,  SE_RPRN,  SE_SCLN,  SE_DQUO,  KC_TRNS,
+    KC_TRNS,  SE_TILD,  SE_PLUS,  SE_LBRC,  SE_RBRC,  SE_PERC,  SE_AT,    SE_COLN,  SE_COMM,  SE_DOT,   SE_QUOT,  KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
+  ),
 
-[4] = LAYOUT_planck_mit(
-    _______,  QK_BOOT,  _______,  _______,  _______,  _______,  _______,  DF(0),    DF(1),    _______,  _______,  _______,
-    _______,  RM_TOGG,  RM_HUEU,  RM_SATU,  RM_VALU,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-    _______,  RM_NEXT,  RM_HUED,  RM_SATD,  RM_VALD,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-    _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,  _______,  _______   
-)
+  [FUN] = LAYOUT_planck_mit(
+    KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_TRNS,
+    KC_TRNS,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_F11,   KC_F12,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_TRNS,
+    KC_TRNS,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
+  ),
+
+  [SYS] = LAYOUT_planck_mit(
+    KC_TRNS,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_TRNS,
+    KC_TRNS,  QK_BOOT,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_TRNS,
+    KC_TRNS,  RM_TOGG,  RM_HUEU,  RM_SATU,  RM_VALU,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
+  )
 
 };
+
+const uint16_t PROGMEM combo_capsword[] = {KC_B, KC_N, COMBO_END};
+const uint16_t PROGMEM combo_syslayer[] = {THUMB_R0, THUMB_R1, COMBO_END};
+combo_t key_combos[] = {
+  COMBO(combo_capsword, CW_TOGG),
+  COMBO(combo_syslayer, MO(SYS)),
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+  case SE_TILD:
+    if (record->event.pressed) {
+      register_code(KC_RALT);
+      tap_code(SE_DIAE);
+      unregister_code(KC_RALT);
+      tap_code(KC_SPC);
+    }
+    return false;
+  case SE_CIRC:
+    if (record->event.pressed) {
+      register_code(KC_LSFT);
+      tap_code(SE_DIAE);
+      unregister_code(KC_LSFT);
+      tap_code(KC_SPC);
+    }
+    return false;
+  }
+  return true;
+}
