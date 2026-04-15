57fb1c178165f89440fe12b09438dcdae5e62545
Claes Hallström <hallstrom.claes@gmail.com>
2026-04-15 23:12:32 +0200

    crkbd: use the ex2 layout

--- keyboards/crkbd/rev4_1/mini/keymaps/claha/keymap.c
+++ keyboards/crkbd/rev4_1/mini/keymaps/claha/keymap.c
@@ -21,46 +21,46 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "claha.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [BASE] = LAYOUT_split_3x5_3(
-    KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,
-    HOME_A,   HOME_S,   HOME_D,   HOME_F,   KC_G,     KC_H,     HOME_J,   HOME_K,   HOME_L,   HOME_SCLN,
-    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,
-                        THUMB_L2, THUMB_L1, THUMB_L0, THUMB_R0, THUMB_R1, THUMB_R2
+  [BASE] = LAYOUT_split_3x5_3_ex2(
+    KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_NO,    KC_NO,    KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,
+    HOME_A,   HOME_S,   HOME_D,   HOME_F,   KC_G,     KC_NO,    KC_NO,    KC_H,     HOME_J,   HOME_K,   HOME_L,   HOME_SCLN,
+    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                         KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,
+                                  THUMB_L2, THUMB_L1, THUMB_L0, THUMB_R0, THUMB_R1, THUMB_R2
   ),
 
-  [NAV] = LAYOUT_split_3x5_3(
-    KC_ESC,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
-    KC_LSFT,  KC_LCTL,  KC_LGUI,  KC_LALT,  KC_NO,    KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, KC_NO,
-    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   KC_DEL,
-                        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
+  [NAV] = LAYOUT_split_3x5_3_ex2(
+    KC_ESC,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+    KC_LSFT,  KC_LCTL,  KC_LGUI,  KC_LALT,  KC_NO,    KC_NO,    KC_NO,    KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, KC_NO,
+    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                        KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   KC_DEL,
+                                  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
   ),
 
-  [NUM] = LAYOUT_split_3x5_3(
-    KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,
-    KC_LSFT,  KC_NUHS,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_LALT,  KC_LGUI,  KC_LCTL,  KC_LSFT,
-    KC_RALT,  KC_DOT,   KC_RBRC,  KC_PLUS,  KC_NO,    KC_NO,    KC_LBRC,  KC_QUOT,  KC_SCLN,  KC_RALT,
-                        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
+  [NUM] = LAYOUT_split_3x5_3_ex2(
+    KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_NO,    KC_NO,    KC_6,     KC_7,     KC_8,     KC_9,     KC_0,
+    KC_LSFT,  KC_NUHS,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_LALT,  KC_LGUI,  KC_LCTL,  KC_LSFT,
+    KC_RALT,  KC_DOT,   KC_RBRC,  KC_PLUS,  KC_NO,                        KC_NO,    KC_LBRC,  KC_QUOT,  KC_SCLN,  KC_RALT,
+                                  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
   ),
 
-  [SYM] = LAYOUT_split_3x5_3(
-    SE_BSLS,  SE_LABK,  SE_RABK,  SE_MINS,  SE_PIPE,  SE_CIRC,  SE_LCBR,  SE_RCBR,  SE_DLR,   SE_QUES,
-    SE_EXLM,  SE_ASTR,  SE_SLSH,  SE_EQL,   SE_AMPR,  SE_HASH,  SE_LPRN,  SE_RPRN,  SE_SCLN,  SE_DQUO,
-    SE_TILD,  SE_PLUS,  SE_LBRC,  SE_RBRC,  SE_PERC,  SE_AT,    SE_COLN,  SE_COMM,  SE_DOT,   SE_QUOT,
-                        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
+  [SYM] = LAYOUT_split_3x5_3_ex2(
+    SE_BSLS,  SE_LABK,  SE_RABK,  SE_MINS,  SE_PIPE,  KC_NO,    KC_NO,    SE_CIRC,  SE_LCBR,  SE_RCBR,  SE_DLR,   SE_QUES,
+    SE_EXLM,  SE_ASTR,  SE_SLSH,  SE_EQL,   SE_AMPR,  KC_NO,    KC_NO,    SE_HASH,  SE_LPRN,  SE_RPRN,  SE_SCLN,  SE_DQUO,
+    SE_TILD,  SE_PLUS,  SE_LBRC,  SE_RBRC,  SE_PERC,                      SE_AT,    SE_COLN,  SE_COMM,  SE_DOT,   SE_QUOT,
+                                  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
   ),
 
-  [FUN] = LAYOUT_split_3x5_3(
-    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,
-    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_F11,   KC_F12,   KC_NO,    KC_NO,    KC_NO,    KC_NO,
-    KC_NO,    KC_N,     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
-                        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
+  [FUN] = LAYOUT_split_3x5_3_ex2(
+    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_NO,    KC_NO,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,
+    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_F11,   KC_NO,    KC_NO,    KC_F12,   KC_NO,    KC_NO,    KC_NO,    KC_NO,
+    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                        KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+                                  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
   ),
 
-  [SYS] = LAYOUT_split_3x5_3(
-    QK_BOOT,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
-    RM_TOGG,  RM_HUEU,  RM_SATU,  RM_VALU,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
-    RM_NEXT,  RM_HUED,  RM_SATD,  RM_VALD,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
-                        KC_NO,    KC_TRNS,  KC_NO,    KC_NO,    KC_NO,    KC_NO
+  [SYS] = LAYOUT_split_3x5_3_ex2(
+    QK_BOOT,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+    RM_TOGG,  RM_HUEU,  RM_SATU,  RM_VALU,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+    RM_NEXT,  RM_HUED,  RM_SATD,  RM_VALD,  KC_NO,                        KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+                                  KC_NO,    KC_TRNS,  KC_NO,    KC_NO,    KC_NO,    KC_NO
   )
 };
 
